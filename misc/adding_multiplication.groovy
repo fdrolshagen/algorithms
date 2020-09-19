beb90677181fe100965fc0d153ce061cbf3a11f4
fdrolshagen <29797989+fdrolshagen@users.noreply.github.com>
2020-09-20 00:37:09 +0200

    adding/multiplying with +-1

---
+++ misc/adding_multiplication.groovy
@@ -0,0 +1,14 @@
+//adding and multiplying a and b by only using +-1
+
+def add(a, b) {
+    if(b == 0) return a
+    return add(a, b - 1) + 1
+}
+
+def mult(a, b) {
+    if((a == 0) || (b == 0)) return 0
+    if(b == 1) return a
+    return add(mult(a, b - 1), a)
+}
+
+println mult(12, 12)
